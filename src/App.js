43e182603d1e94b6a63e97416dd2dc7ffeb3944e
maxbhs <max.melendez.llorens@gmail.com>
2018-11-06 16:29:44 +0100

    Debuging kube dns

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 	};
 
 	sendExpr() {
-		fetch('http://calc-webapp-lb/calculator', {
+		fetch('http://calc-logic-lb/calculator', {
 			method: 'POST',
       			headers: {
 				'Content-Type': 'application/json'
